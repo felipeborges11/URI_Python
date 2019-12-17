9592714af7376a155cfd28ca48e53d2ab65f18a6
felipebsilva11 <felipebsilva5@gmail.com>
2019-12-17 20:35:39 -0300

    add: ex 1035

---
+++ 1035.py
@@ -0,0 +1,10 @@
+arr = input()
+var = list(map(int,arr.split(' ')))
+A = var[0]
+B = var[1]
+C = var[2]
+D = var[3]
+if(B > C and D > A and (C + D) > (A + B) and C >= 0 and D >= 0 and A % 2 == 0):
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
