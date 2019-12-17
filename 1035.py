c4d4a2ca25233cc27ec2194ba98d0886b22e6956
felipebsilva11 <felipebsilva5@gmail.com>
2019-12-17 20:38:47 -0300

    add: enunciado 1035

--- 1035.py
+++ 1035.py
@@ -1,10 +1,26 @@
+'''
+Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+
+Entrada
+Quatro números inteiros A, B, C e D.
+
+Saída
+Mostre a respectiva mensagem após a validação dos valores.
+
+Exemplo de Entrada      	Exemplo de Saída
+
+5 6 7 8                     Valores nao aceitos
+
+2 3 2 6                     Valores aceitos
+'''
+
 arr = input()
 var = list(map(int,arr.split(' ')))
 A = var[0]
 B = var[1]
 C = var[2]
-D = var[3]
+ var[3]
 if(B > C and D > A and (C + D) > (A + B) and C >= 0 and D >= 0 and A % 2 == 0):
     print('Valores aceitos')
 else:
-    print('Valores nao aceitos')
+    print('Valores nao aceitos')D =
