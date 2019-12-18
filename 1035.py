0f3130eceeb44f7006367e7b348d324ebdf94d09
felipebsilva11 <felipebsilva5@gmail.com>
2019-12-17 21:34:51 -0300

    add: exercicios

--- 1035.py
+++ 1035.py
@@ -9,7 +9,7 @@ var = list(map(int,arr.split(' ')))
 A = var[0]
 B = var[1]
 C = var[2]
- var[3]
+D = var[3]
 if(B > C and D > A and (C + D) > (A + B) and C >= 0 and D >= 0 and A % 2 == 0):
     print('Valores aceitos')
 else:
